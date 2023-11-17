0439a51ff563eb388ef0b4745eec825cf53264ff
Rafael Martins <123112122+frontmartins@users.noreply.github.com>
2023-11-17 02:54:07 -0300

    classe contato

--- src/main/java/com/reconnect/model/Contato.java
+++ src/main/java/com/reconnect/model/Contato.java
@@ -11,6 +11,14 @@ public class Contato {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false)
+	private String nome;
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
 	@Column(name="mensagem")
 	private String mensagem;
 
@@ -21,4 +29,43 @@ public class Contato {
 	public void setMensagem(String mensagem) {
 		this.mensagem = mensagem;
 	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Contato(Long id, String nome, String email, String mensagem) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.mensagem = mensagem;
+	}
+
+	public Contato() {
+		super();
+	}
+	
+	
+	
 }
