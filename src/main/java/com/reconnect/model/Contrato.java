c34a722eec1c7c4647e0d7f23fb7c9535b822b4a
Patricia Ferrer <124654139+PatsFerrer@users.noreply.github.com>
2023-11-18 19:40:49 -0300

    data sql

--- src/main/java/com/reconnect/model/Contrato.java
+++ src/main/java/com/reconnect/model/Contrato.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
